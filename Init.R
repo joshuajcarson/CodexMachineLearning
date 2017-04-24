a45087be58b720f49f2cd33dd0c3d0185be71c81
joshuajcarson <joshuajcarson@gmail.com>
2017-04-24 06:43:05 -0500

    Added the bit to make starter decks

--- Init.R
+++ Init.R
@@ -1,4 +1,21 @@
 library('data.table')
 require(data.table)
 
-allCardData <- rbind(rbind(fread("data/Neutral.tsv"), fread("data/Red.tsv"), fread("data/Green.tsv"), fread("data/Black.tsv"), fread("data/Blue.tsv"), fread("data/White.tsv")), fread("data/Purple.tsv"), fill = TRUE)
+#load all card data
+allCardData <- rbind(fread("data/Neutral.tsv"), fread("data/Red.tsv"), fread("data/Green.tsv"), fread("data/Black.tsv"), fread("data/Blue.tsv"), fread("data/White.tsv"), fread("data/Purple.tsv"), fread("data/Heroes.tsv"), fill = TRUE)
+
+#make the starter deck for Bashing and Finesse
+neutralStarterDeck <- allCardData[which(allCardData$starting_zone == 'deck' & allCardData$color == 'Neutral'),]
+starter1 <- neutralStarterDeck
+starter2 <- neutralStarterDeck
+starter1$player = '1'
+starter2$player = '2'
+currentGame <- rbind(starter1, starter2)
+rm(neutralStarterDeck, starter1, starter2)
+
+#Setup the 'currentGame' zone which will have the current state of the game at all times
+#TODO actually deal out cards instead of just putting one card to the hand of one of the players
+currentGame$current_zone = currentGame$starting_zone
+currentGame$current_zone[currentGame$player=='1'&currentGame$current_zone=='deck'] <- 'deck'
+
+currentGame$current_zone[which(currentGame$player=='1'&currentGame$current_zone=='deck')[2]] <- 'hand'
